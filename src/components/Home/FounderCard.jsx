41b398acb8a52627cbec3f75cf77a68453030841
Abhishek-69889 <abhit0396@gmail.com>
2026-09-15 18:40:52 +0530

    Added founders Image

--- src/components/Home/FounderCard.jsx
+++ src/components/Home/FounderCard.jsx
@@ -1,10 +1,11 @@
 import {Quote} from 'lucide-react'
+import founder from "../../assets/images/founder_image.jpg"
 
 const FounderCard = () => {
   return (
     <div className='absolute -bottom-8 -right-2 lg:-right-8 bg-white rounded-2xl p-5 shadow-[0_20px_50px_rgba(0,0,0,0.15)] border border-zinc-100  '>
             <div className='flex gap-3  '>
-              <img src="" className='w-12 h-12 rounded-full object-cover' alt="founder" />
+              <img src={founder} className='w-12 h-12 rounded-full object-cover' alt="founder" />
               <div>
                 <p className='font-bold text-zinc-900 leading-none'>Raju Tiwari</p>
                 <p className='text-xs text-zinc-500 mt-1'>Founder, Abhishek Traders</p>
